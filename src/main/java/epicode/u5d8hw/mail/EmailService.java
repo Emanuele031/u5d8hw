05124e93a4e57d66823e62241bb5657a7df5a6a2
emanuele <lazzaroemanuele466@gmail.com>
2025-02-13 16:37:54 +0100

    add

---
+++ src/main/java/epicode/u5d8hw/mail/EmailService.java
@@ -0,0 +1,36 @@
+package epicode.u5d8hw.mail;
+
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.hibernate.pretty.MessageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        //SimpleMailMessage message = new SimpleMailMessage();
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body, true);
+        helper.setFrom("your-email@gmail.com");
+
+        mailSender.send(message);
+        System.out.println("Email inviata con successo a " + to);
+    }
+}
+
